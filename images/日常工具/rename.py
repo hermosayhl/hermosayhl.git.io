0f24f2e68f9444bf6a06a3b1a037bd1c2e8a75f0
hermosayhl <2535894692@qq.com>
2020-01-12 12:11:50 +0800

    Site updated: 2020-01-12 12:11:46

--- images/日常工具/rename.py
+++ images/日常工具/rename.py
@@ -4,4 +4,4 @@ import os
 file_names = os.listdir('./')
 file_names = [it for it in file_names if('py' not in it)]
 for it in file_names:
-	os.rename(it, it.replace('(', '-').replace(')', '-').replace(' ', ''))
+	os.rename(it, it.replace('(', '-').replace(')', '-').replace(' ', '').replace('（', '-').replace('）', '-'))
