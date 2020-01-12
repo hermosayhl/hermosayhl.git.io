2be94f8d3ee82c22f41577642b3be513d499b445
hermosayhl <2535894692@qq.com>
2020-01-12 11:03:04 +0800

    Site updated: 2020-01-12 11:03:01

---
+++ images/algorithms/排序/rename.py
@@ -0,0 +1,7 @@
+import os
+
+
+file_names = os.listdir('./')
+file_names = [it for it in file_names if('py' not in it)]
+for it in file_names:
+	os.rename(it, it.replace('(', '-').replace(')', '-').replace(' ', ''))
